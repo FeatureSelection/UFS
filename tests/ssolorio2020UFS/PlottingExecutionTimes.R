9dd53fafdb47188ae41941603f2220099fcc799f
Venustiano <v.soancatl.aguilar@rug.nl>
2020-12-27 21:48:13 +0100

    Plotting Features vs time

---
+++ tests/ssolorio2020UFS/PlottingExecutionTimes.R
@@ -0,0 +1,34 @@
+# Plotting algorithm running time  from
+
+# A systematic evaluation of filter Unsupervised Feature Selection methods
+# Author links open overlay panelSaúlSolorio-Fernández
+# J.Ariel Carrasco-OchoaJosé Fco.Martínez-Trinidad
+
+library(data.table)
+library(ggplot2)
+
+# Tables 19 and 20
+dtr <- read.csv("/home/venus/Downloads/DSUFS75-time.csv",sep=',')
+
+dtr <- as.data.table(dtr)
+
+# Tables 1 and 2
+dtz <- read.table("/home/venus/Downloads/DSUFS75-size.csv",sep=',',header=TRUE)
+dtz <- as.data.table(dtz)
+
+# Converting from wide to long format
+ldtr <- melt(dtr, id.vars = c("Dataset"), variable.name = "method",value.name = "rtime")
+
+setkey(ldtr,Dataset)
+setkey(dtz,Dataset)
+
+# Mergind tables
+# https://rstudio-pubs-static.s3.amazonaws.com/52230_5ae0d25125b544caab32f75f0360e775.html
+ldtr <- dtz[ldtr]
+
+# http://zevross.com/blog/2019/04/02/easy-multi-panel-plots-in-r-using-facet_wrap-and-facet_grid-from-ggplot2/
+ggplot(ldtr,aes(x=N_of_Features,y=rtime)) + geom_point() + 
+  stat_smooth(method=lm,se=F,lwd=1.2,fullrange=T)+theme_bw() +
+  facet_wrap(~method)
+
+
